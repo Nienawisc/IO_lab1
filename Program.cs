dfab48f52626f261ca1bbfc8a30b8d987569862b
Robert Greliński <robert.grelinski@student.put.poznan.pl>
2017-10-24 23:41:54 +0200

    Zadania

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static object lockObj = new object();
+
+        
+        static void Main(string[] args)
+        {
+
+            ThreadPool.QueueUserWorkItem(Server);
+            ThreadPool.QueueUserWorkItem(Client);
+            ThreadPool.QueueUserWorkItem(Client);
+            while (true) ;
+        }
+        static void Client(Object stateInfo)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2048));
+            while (true)
+            {
+                string wiadomosc = Console.ReadLine();
+                byte[] message = new ASCIIEncoding().GetBytes(wiadomosc);
+                client.GetStream().Write(message, 0, message.Length);
+                writeConsoleMessage(wiadomosc, ConsoleColor.Green);
+                if (wiadomosc == "END") break;
+            }
+            client.Close();
+        }
+        static void Server(Object stateInfo)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 2048);
+            server.Start();
+            while (true)
+            {
+                writeConsoleMessage("Waiting for a connection... ", ConsoleColor.Red);
+                TcpClient c = server.AcceptTcpClient();
+                writeConsoleMessage("Connected! ", ConsoleColor.Red);
+                ThreadPool.QueueUserWorkItem(ClientServer, new object[] { c });
+            }
+        }
+        static void ClientServer(Object stateInfo)
+        {
+            TcpClient c = (TcpClient)((object[])stateInfo)[0];
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                c.GetStream().Read(buffer, 0, 1024);
+                string result = Encoding.UTF8.GetString(buffer);
+                result = result.Split((char)0)[0];
+                writeConsoleMessage(("Otrzymałem wiadomość: " + result), ConsoleColor.Red);
+                if (result == "END") break;
+            }
+        }
+        static void writeConsoleMessage(string message, ConsoleColor color)
+        {
+            lock (lockObj)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("IO-lab1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IO-lab1")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("5bcd8d1b-4d2d-4fe9-996e-65ae822a2b68")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
